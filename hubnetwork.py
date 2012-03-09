4e34ebf854db7d6e1397442d4f4552f3ac7fe6f0
Simon Carr <simonjcarr@gmail.com>
2012-03-09 14:59:26 +0000

    Added hubnetwork.py to manage hub socket server

---
+++ hubnetwork.py
@@ -0,0 +1,8 @@
+#Created by Simon Carr
+#simonjcarr@gmail.com
+#09/03/2012
+
+#hubnetwork.py is a file that runs a socket daemon to monitor the network for traffic between the hub and remotes
+
+#ToDo
+#Create sockets server to allow remotes to connect
