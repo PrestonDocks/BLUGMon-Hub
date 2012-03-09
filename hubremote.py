694e1e2fbfbf2ff81750a8288a0d1fa9196cc028
Simon Carr <simonjcarr@gmail.com>
2012-03-09 13:25:59 +0000

    Created hubremote.py and renamed jobs->tasks
    created hubremote.py with empty functions()
    
    renamed jobs folder to tasks

---
+++ hubremote.py
@@ -0,0 +1,38 @@
+
+
+class HubRemote(object):
+    def __init__(self):
+        pass
+    
+    def rmtcreate(self):
+        pass
+    
+    def rmtlist(self):
+        pass
+    
+    def rmtdelete(self):
+        pass
+    
+    def addModule(self):
+        pass
+    
+    def rmModule(self):
+        pass
+    
+    def lsmodule(self):
+        pass
+    
+    def lscmd(self):
+        pass
+    
+    def addcmd(self):
+        pass
+    
+    def rmcmd(self):
+        pass
+    
+    
+    
+    
+    
+        
